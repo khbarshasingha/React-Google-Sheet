e8ca65995e38fe54c21fd71c6e79223fe72b9017
Kh Barsha Singha <khbarshasingha@gmail.com>
2021-02-20 04:15:11 +0530

    created form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Button, Form, Container, Header } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Header as="h2">REact Google Sheets!</Header>
+      <Form>
+        <Form.field>
+          <label>Name</label>
+          <input placeholder="Enter your name" />
+        </Form.field>
+        <Form.field>
+          <label>Age</label>
+          <input placeholder="Enter your Age" />
+        </Form.field>
+        <Form.field>
+          <label>Salary</label>
+          <input placeholder="Enter your salary" />
+        </Form.field>
+        <Form.field>
+          <label>Hobby</label>
+          <input placeholder="Enter your Hobby" />
+        </Form.field>
+        <Button color="blue" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
